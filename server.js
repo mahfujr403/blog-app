ed3acfe70a277761c32b6aeb127e0ea1f30798eb
Mahfuj <mahfujr403@gmail.com>
2025-12-19 00:01:08 +0600

    basic server setup done

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! it is working fine.');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
