e2acdd2fbb6b8653a9cc947092e271af9ba21328
Mahfuj <mahfujr403@gmail.com>
2025-12-20 03:04:06 +0600

    blog model designed

---
+++ models/blogModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    category: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    isPublished: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+export { Blog };
--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,37 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-
-
 const userSchema = new mongoose.Schema({
-    name : String,
-    email : { type: String, unique: true },
-    password : String,
-    createdAt : { type: Date, default: Date.now }
-})
-
-
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
 // Hash password before saving
-userSchema.pre('save', async function() {
-    if (!this.isModified('password')) return;
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword) {
+userSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
-}
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export { User };
